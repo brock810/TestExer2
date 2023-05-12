24b2b5019c75a167840782223f16ea7226778f8e
brock810 <130688727+brock810@users.noreply.github.com>
2023-05-12 13:32:34 -0500

    another file

--- PalindromeExercise/PalindromeExercise/Program.cs
+++ PalindromeExercise/PalindromeExercise/Program.cs
@@ -2,11 +2,22 @@
 
 namespace PalindromeExercise
 {
-    class Program
+    public class WordSmith
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+        }
+            public bool IsAPalindrome(string word)
+            {
+                
+                return word == Reverse(word);
+            }
+
+            private string Reverse(string word)
+            {
+                var charArray = word.ToCharArray();
+                Array.Reverse(charArray);
+                return new string(charArray);
+            }
         }
     }
-}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,24 @@
-using System;
+using PalindromeExercise;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("hello", false)]
+        [InlineData("1221", true)]
+        public void TestIsAPalindrome(string word, bool expected)
         {
+            // Arrange
+            var wordSmith = new WordSmith();
 
+            // Act
+            var actual = wordSmith.IsAPalindrome(word);
+
+            // Assert
+            Assert.Equal(expected, actual);
         }
     }
 }
